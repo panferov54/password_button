4ab2355de1302b11ca622e68d74d7a6020d25de6
Mikhail Panferov <panferovm@gmail.com>
2022-01-13 15:29:43 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.querySelector(".pwd input");
+const eye = document.querySelector(".pwd .fa-eye-slash");
+const lock = document.querySelector(".pwd .fa-lock");
+const ioverlay = document.querySelector(".pwd .overlay");
+
+eye.addEventListener("click",()=>{
+if (input.type === "password"){
+    input.type= "text";
+    eye.classList.remove("fa-eye-slash");
+    eye.classList.add("fa-eye")
+    setTimeout(()=>{
+        lock.getElementsByClassName.color = "#111625";
+    },500)
+}else {
+    input.type = "password";
+    eye.classList.remove("fa-eye")
+    eye.classList.add("fa-eye-slash");
+    lock.style.color="#dbdbdb";
+}
+overlay.classList.toggle("overlaycover")
+});
